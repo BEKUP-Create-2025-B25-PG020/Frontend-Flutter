496709a78a58246246bce003e2f9fd2e3c61ede8
azra <91065112+muhammadazra4503@users.noreply.github.com>
2025-10-22 16:19:50 +0700

    Add favorite feature

--- lib/feature/provider/food_detail_provider.dart
+++ lib/feature/provider/food_detail_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:mantra_application/common/static/food_detail_result_state.dart';
+import 'package:mantra_application/core/data/model/food_detail.dart';
 import 'package:mantra_application/core/data/service/http_service.dart';
 
 class FoodDetailProvider extends ChangeNotifier {
@@ -22,5 +23,37 @@ class FoodDetailProvider extends ChangeNotifier {
     }
 
     notifyListeners();
+  }
+
+  Future<void> likeOrUnlikeFood(int foodId, bool isCurrentlyLiked) async {
+    if (_state is! FoodDetailLoadedState) return;
+
+    try {
+      final response = isCurrentlyLiked
+          ? await httpService.unlikeFood(foodId)
+          : await httpService.likeFood(foodId);
+
+      final currentFood = (_state as FoodDetailLoadedState).data;
+
+      final updatedFood = FoodDetail(
+        id: currentFood.id,
+        name: currentFood.name,
+        shortDescription: currentFood.shortDescription,
+        longDescription: currentFood.longDescription,
+        foodHistory: currentFood.foodHistory,
+        interestingFacts: currentFood.interestingFacts,
+        mainImageUrl: currentFood.mainImageUrl,
+        isFeatured: currentFood.isFeatured,
+        region: currentFood.region,
+        category: currentFood.category,
+        likesCount: response.data.likesCount,
+        galleries: currentFood.galleries,
+      );
+      _state = FoodDetailLoadedState(updatedFood);
+      notifyListeners();
+    } catch (e) {
+      _state = FoodDetailErrorState(e.toString());
+      notifyListeners();
+    }
   }
 }

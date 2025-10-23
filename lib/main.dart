d7101fe290d542ce2f4262216fe4e6a8a4d4a95d
GulaminHalim <halimsrgr@gmail.com>
2025-10-23 17:09:49 +0700

    Add feature/onboarding_screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:mantra_application/common/static/navigation_route.dart';
 import 'package:mantra_application/common/style/theme/mantra_theme.dart';
 import 'package:mantra_application/core/data/service/http_service.dart';
 import 'package:mantra_application/feature/detail/detail_screen.dart';
+import 'package:mantra_application/feature/onboarding/onboarding_screen.dart';
 import 'package:mantra_application/feature/settings/screen/about_screen.dart';
 import 'package:mantra_application/feature/settings/screen/settings_screen.dart';
 import 'package:mantra_application/feature/provider/explore_list_provider.dart';
